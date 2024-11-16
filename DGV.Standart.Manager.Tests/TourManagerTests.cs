3ddb5e41e9219c591abeb205013dbdb571e75c36
леонид коробков <korobkov-leo@mail.ru>
2024-11-16 14:19:49 +0300

    storage test

---
+++ DGV.Standart.Manager.Tests/TourManagerTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using DGV.Standart.Contracts;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+
+namespace DGV.Standart.Manager.Tests
+{
+    /// <summary>Тесты для <see cref="TourManager"/></summary>
+    public class TourManagerTests
+    {
+        private readonly ITourManager tourManager;
+        private readonly Mock<ITourStorage> storageMock;
+        private readonly Mock<ILogger> loggerMock;
+
+        /// <summary>
+        /// Получение пустых данных из хранилища
+        /// </summary>
+        [Fact]
+        public async Task GetAllAsyncShouldReturnEmpty()
+        {
+            // Arrange
+            storageMock.Setup(x => x.GetAllToursAsync());
+
+            // Act
+            var result = await tourManager.GetAllToursAsync();
+
+            // Assert
+            result.Should().BeNull();
+
+            storageMock.Verify(x => x.GetAllToursAsync(), Times.Once);
+            storageMock.VerifyNoOtherCalls();
+
+            loggerMock.VerifyNoOtherCalls();
+        }
+
+    }
+}
