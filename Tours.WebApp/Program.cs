cc2e6209dde47865bf266088f1c6ca2c3b6284aa
леонид коробков <korobkov-leo@mail.ru>
2024-12-02 18:14:23 +0300

    TourController test

--- Tours.WebApp/Program.cs
+++ Tours.WebApp/Program.cs
@@ -1,4 +1,28 @@
+﻿using DGV.Standart.Manager;
+using DGV.Storage.Database;
+using Serilog.Extensions.Logging;
+using Serilog;
+using DGV.Standart.Contracts;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Настройка Serilog
+var serilogLogger = new LoggerConfiguration()
+    .MinimumLevel.Verbose()
+    .WriteTo.Seq("http://localhost:5341/", apiKey: "mudhnSNLQJrgaLSZPuuw")
+    .CreateLogger();
+
+// Установка логгера Serilog
+builder.Host.UseSerilog(serilogLogger);
+
+// Регистрация зависимостей
+builder.Services.AddSingleton<ILoggerFactory>(new SerilogLoggerFactory(serilogLogger));
+builder.Services.AddSingleton<Microsoft.Extensions.Logging.ILogger>(sp => sp.GetRequiredService<ILoggerFactory>().CreateLogger("aspnet-mvc"));
+
+// Регистрация хранилища и менеджера
+builder.Services.AddSingleton<ITourStorage, DBTour>();
+builder.Services.AddTransient<TourManager>();
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -22,6 +46,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Tour}/{action=Index}/{id?}");
 
 app.Run();
